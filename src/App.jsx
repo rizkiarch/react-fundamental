0843ab440dac12885b14c9faf14ecd6480a30197
rizkiarch <muha20098ti@student.nurulfikri.ac.id>
2024-07-19 23:39:41 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
-import Home from './pages/index'
+import { GlobalContext } from './context'
+import { router } from './routers'
 
 function App() {
+  const user = {
+    username: "Ikay"
+  }
 
   return (
     <>
       <div className='App'>
-        <Home/>
+      <GlobalContext.Provider value={user} >
+        <RouterProvider router={router}></RouterProvider>
+      </GlobalContext.Provider>
       </div>
     </>
   )
