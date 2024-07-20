062e3e26203c74d357414b7ee6ebe77264557f3a
rizkiarch <muha20098ti@student.nurulfikri.ac.id>
2024-07-20 19:30:06 +0700

    up

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,11 +1,23 @@
 import { Link, useLoaderData } from "react-router-dom";
+import Search from "../../components/Search";
+import { useState } from "react";
 
 function Blog() {
-    const posts = useLoaderData();
+    const initialPosts = useLoaderData();
+    const [posts, setPosts] = useState(initialPosts);
+    const [totalPosts, setTotalPosts] = useState(initialPosts.length);
 
+    const onSearchChange = (value) => {
+        const filteredPosts = initialPosts.filter((item) =>
+          item.title.includes(value)
+        );
+        setPosts(filteredPosts);
+        setTotalPosts(filteredPosts.length);
+      };
     return (
         <>
          <h3>External Post</h3>
+         <Search onSearchChange={onSearchChange} totalPosts={totalPosts}/>
         {posts.map((item, index)=>(
             <div key={index}>
                 <Link to={`/blog/${item.id}`} >{item.title}</Link>
